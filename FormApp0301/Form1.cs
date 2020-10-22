91c67ed9e07f570fd8a3f381ba9a70a39344ea1d
botshen <363088847@qq.com>
2020-10-22 18:00:05 +0800

    添加项目文件。

---
+++ FormApp0301/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApp0301
+{
+    public partial class Form1 : Form
+    {
+        private Button testButton = new Button();
+        public Form1()
+        {
+            InitializeComponent();
+
+            //自己添加一个按钮
+            Controls.Add(testButton);
+            testButton.Text = "我的测试";
+        }
+    }
+}
